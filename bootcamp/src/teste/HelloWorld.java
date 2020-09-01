a7ca2ce430b5689a07fd100cfc1cf9cb1bcbd658
Felipe Santos <felipe.santos.freitas@outlook.com>
2020-08-31 21:16:21 -0300

    adicionando uma condição

--- bootcamp/src/teste/HelloWorld.java
+++ bootcamp/src/teste/HelloWorld.java
@@ -2,7 +2,14 @@ package teste;
 
 public class HelloWorld {
 	public static void main(String[] args) {
+		int i = 0;
+		
 		System.out.println("Hello Linux!");
+		if(i > -1) {
+			System.out.println("teste");
+		} else {
+			System.out.println("fail");
+		}
 	}
 
 }
